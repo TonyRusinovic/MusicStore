5c5f0bfeb54c5323b43cf346e371ea4bfbced80c
Tony Rusinovic <tony_rusinovic@hotmail.com>
2019-04-08 12:19:38 +0200

    After creating album model

---
+++ MyMVC3/Models/Album.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMVC3.Models
+{
+    public class Album
+    {
+        public virtual int AlbumID { get; set; }
+        public virtual int GenreID { get; set; }
+        public virtual int ArtistID { get; set; }
+        public virtual string Title { get; set; }
+        public virtual double Price { get; set; }
+        public virtual Genre Genre { get; set; }
+        public virtual Artist Artist { get; set; }
+
+    }
+
+    public class Artist
+    {
+        public virtual int ArtistID { get; set; }
+        public virtual string Name { get; set; }
+    }
+
+    public class Genre
+    {
+        public virtual int GenreID { get; set; }
+        public virtual string Name { get; set; }
+        public virtual List<Album> Albums { get; set; }
+    }
+}
